6adfe5421281526abc8c653760825c97db8021cc
Thanga Ganapathy <ganapathy888@gmail.com>
2021-08-24 16:02:03 +0530

    Split str arguments for parsing

--- src/parser.ts
+++ src/parser.ts
@@ -7,6 +7,13 @@ function getValue(str: string): unknown {
     return Number(str);
   }
 
+  if (RegExp(/^["'][^"']*["']$/).test(str)) {
+    const regExpRes = new RegExp(/^["']([^"']*)["']$/).exec(str);
+    if (regExpRes) {
+      return regExpRes[1];
+    }
+  }
+
   return str;
 }
 
@@ -44,8 +51,41 @@ function isShortOptVal(str: string): boolean {
   return RegExp(/^(?:-{1})(?:[a-zA-Z]+)=(?:.*)$/).test(str);
 }
 
+function splitStr(args: string): string[] {
+  const out = [];
+  let str = '';
+
+  for (let i = 0; i < args.length; i++) {
+    const char = args[i];
+
+    if (char === '"' || char === "'") {
+      const regExpResult = new RegExp(/["']([^"']*)["']/).exec(
+        args.substring(i)
+      );
+      if (regExpResult) {
+        str += regExpResult[0];
+        i = i + regExpResult[0].length;
+        continue;
+      }
+    }
+
+    if (char === ' ') {
+      out.push(str);
+      str = '';
+      continue;
+    }
+
+    str += char;
+  }
+
+  out.push(str);
+  console.log(out);
+
+  return out;
+}
+
 function parser(args: string | string[]): ObjType {
-  const curArgs = typeof args === 'string' ? args.trim().split(' ') : args;
+  const curArgs = typeof args === 'string' ? splitStr(args.trim()) : args;
   const outObj: ObjType = { args: [], opts: {} };
 
   for (let i = 0; i < curArgs.length; i++) {
